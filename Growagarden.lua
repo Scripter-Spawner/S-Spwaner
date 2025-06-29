73367a195d72f5544bb2a0641d8de54f1d27ec97
Scripter-Spawner <azmibarok13@gmail.com>
2025-06-29 10:06:42 +0700

    Update Growagarden.lua

--- Growagarden.lua
+++ Growagarden.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripter-Spawner/S-Spwaner/refs/heads/main/Nolaghub-Spawner"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripter-Spawner/S-Spwaner/refs/heads/main/Update"))()
